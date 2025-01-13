6171070ac8c3ae7a1b000b30fd812e93094293df
Divyanshu Shekhar <149070310+Divyanshu832@users.noreply.github.com>
2025-01-14 02:57:39 +0530

    Consoles issues

--- src/context/data/myData.jsx
+++ src/context/data/myData.jsx
@@ -141,7 +141,7 @@ function MyState(props) {
         setLoading(false)
       });
       setOrder(ordersArray);
-      console.log(ordersArray)
+      // console.log(ordersArray)
       setLoading(false);
     } catch (error) {
       console.log(error)
@@ -161,7 +161,7 @@ function MyState(props) {
         setLoading(false)
       });
       setUser(usersArray);
-      console.log(usersArray)
+      // console.log(usersArray)
       setLoading(false);
     } catch (error) {
       console.log(error)
